79be78abe573c62d4d36b955a106dbc7360a5c20
audaxss <justinpascualx@gmail.com>
2021-03-22 22:31:36 -1200

    navbar bottom and search bar icon on mobile added

--- app/dist/script/navbar.js
+++ app/dist/script/navbar.js
@@ -1,9 +1,32 @@
 function adjustSubmenuCategoryTop() {
-    const navbarSubmenu = document.querySelector('#submenu');
-    const navbarSubmenuCategory = document.querySelector('#submenu__category');
+    const topNavbar = document.querySelector('#top__navbar');
+    const elementMarginTop = document.querySelector('#marginTop');
 
-    navbarSubmenuCategory.style.marginTop = (navbarSubmenu.offsetHeight - 36)+ 'px';
+    elementMarginTop.style.marginTop = (topNavbar.offsetHeight) + 'px';
 }
 
-// adjustSubmenuCategoryTop();
-// window.addEventListener('resize', adjustSubmenuCategoryTop);
+adjustSubmenuCategoryTop();
+window.addEventListener('resize', adjustSubmenuCategoryTop);
+
+const submenuAnchors = document.querySelectorAll('#submenu__anchors > li');
+const submenuSale = document.querySelectorAll('#submenu__sale > li');
+const overlay = document.querySelector('#overlay');
+
+function backgroundOverlay(element) {
+    element.addEventListener('mouseover', () => {
+        overlay.style.display = 'block';
+    })
+
+    element.addEventListener('mouseout', () => {
+        overlay.style.display = 'none';
+        overlay.removeAttribute('style');
+    })
+}
+
+submenuAnchors.forEach(anchor => {
+    backgroundOverlay(anchor);
+});
+
+submenuSale.forEach(anchor => {
+    backgroundOverlay(anchor);
+});
